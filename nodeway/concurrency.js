c9abbe9b440c84b69d0e4b2277823a7142b87b11
Byrdann Fox <byrdannfox@gmail.com>
2016-04-23 18:03:04 -0400

    playing around with some javascript concurrency stuff =)

---
+++ nodeway/concurrency.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const moment = require('moment');
+
+let timeStampExpensive, timeStampTimer;
+expensive(500);
+function expensive (n) {
+    timeStampExpensive = moment().format();
+    let i = 0;
+    while (++i < n * n) { console.log(i); }
+    return i;
+}
+
+console.log('Hey, when is that expensive function going to stop executing it\'s functionality?');
+
+setTimeout(() => {
+    timeStampTimer = moment().format();
+}, 300);
+let difference = moment.utc(timeStampTimer).diff(moment(timeStampExpensive));
+console.log(`It took ${moment.duration(difference).seconds()} seconds for the event loop to get to this queued task...`);
+
+// let cnt = 0;
