36bdc6018a4b6844380f3d75641c95a2b2bae64b
Byrdann Fox <byrdannfox@gmail.com>
2016-04-29 03:55:21 -0400

    cha cha cha changes

--- nodecure/node.cure.js
+++ nodecure/node.cure.js
@@ -1,127 +1,37 @@
 'use strict';
 
-// for 'HTTPS' make sure you only use SHA256...
-
-// require('ssl-root-cas').inject();
-// require('ssl-root-cas').addFile('my-cert.crt);
-
-// const https = require('https');
-
-// https.get('https://www.amazon.com/', (res) => {
-//     console.log(`Status Code: ${res.statusCode}`);
-//     res.on('data', (d) => {
-//         process.stdout.write(d);
-//     });
-// });
-
-// const tls = require('tls'),
-//     fs = require('fs');
-//
-// let options = {
-//     key  : fs.readFileSync('private.key'),
-//     cert : fs.readFileSync('public.cert')
-// };
-//
-// let server = tls.createServer(options, (res) => {
-//     res.write('Hello World...');
-//     res.pipe(res);
-// }).listen(8000);
-
-// const tls = require('tls'),
-//     fs = require('fs');
-//
-// let options = {
-//     key  : fs.readFileSync('private.key'),
-//     cert : fs.readFileSync('public.cert')
-// };
-//
-// let client = tls.connect(8000, options, () => {
-//     console.log(client.authorized ? 'Authorized' : 'Not authorized');
-// });
-//
-// client.on('data', (data) => {
-//     console.log(data.toString());
-//     client.end();
-// });
-
-// 2048 bit keys at least
-
-// http://www.sitepoint.com/how-to-use-ssltls-with-node-js/
-
-// sudo openssl genrsa -out server.enc.key 2048
-
-// certificate signing request
-// sudo openssl req -new -key server.enc.key -out server.csr
-
-// (optional on self-signing certificates)...
-// remove password protection from the server.enc.key
-// sudo openssl rsa -in server.enc.key -out server.key
-
-// self sign the certificate, but it'll only be good for personal development, not on the web or native devices...
-// sudo openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
-
-// note: use NGINX for all SSL requests and Static content...
-
-// https.createServer(options, (req, res) => {
-//     res.writeHead(200);
-//     res.end('secure, hello world\n');
-// }).listen(3000);
-
 const app = require('express')(),
     cluster = require('cluster'),
-    /* numCPUs = require('os').cpus().length,*/
     https = require('https'),
     morgan = require('morgan'),
     fs = require('fs'),
     PORT = process.env.PORT || 8080,
     options = {
-        // consult deploying node.js book for production SSL key, cert, ca setup...
         key  :  fs.readFileSync('server-key.pem'),
         cert :  fs.readFileSync('server-cert.pem')
     };
 
-// TODO: cluster research
-// if (cluster.isMaster) {
-//
-//     for (let i = 0; i < numCPUs; i++) {
-//         cluster.fork();
-//     }
-//
-//     cluster.on('disconnect', (worker) => {
-//         console.log(`${worker.process.pid} disconnected`);
-//         cluster.fork();
-//     });
-//
-//     cluster.on('exit', (worker, code, signal) => {
-//         console.log(`worker ${worker.process.pid} died`);
-//     });
-//
-// } else {
+// TODO: research more about Strict-Transport-Security.
+app.use((req, res, next) => {
+    let aYear = 60 * 60 * 24 * 365;
+    res.set('Strict-Transport-Security', `max-age=${aYear};includeSubdomains`);
+    next();
+});
 
-    // TODO: research more about Strict-Transport-Security.
-    app.use((req, res, next) => {
-        let aYear = 60 * 60 * 24 * 365;
-        res.set('Strict-Transport-Security', `max-age=${aYear};includeSubdomains`);
-        next();
+// TODO: research morgan logger and decide what we want to be logging for development, testing and production builds.
+// TODO: research how to best timestamp, privatize and organize the storage of log files (they should be securely stored on separate machines).
+// TODO: make sure you understand where an error could possibly happen at any time in the code and handle it.
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+    res.send('Secure...');
+});
+
+const server = https.createServer(options, app).listen(PORT, () => {
+    console.log('Start...');
+    process.on('uncaughtException', (err) => {
+        console.error(err);
+        server.close();
+        cluster.worker.disconnect();
     });
-
-    // TODO: research morgan logger and decide what we want to be logging for development, testing and production builds.
-    // TODO: research how to best timestamp, privatize and organize the storage of log files (they should be securely stored on separate machines).
-    // TODO: make sure you understand where an error could possibly happen at any time in the code and handle it.
-    app.use(morgan('combined'));
-
-    app.get('/', (req, res) => {
-        res.send('Secure...');
-    });
-
-    const server = https.createServer(options, app).listen(PORT, () => {
-        console.log('Start...');
-        // something bad happened =(
-        process.on('uncaughtException', (err) => {
-            console.error(err);
-            server.close();
-            cluster.worker.disconnect();
-        });
-    });
-
-// }
+});
--- nodeway/es6.js
+++ nodeway/es6.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function checkNum (num) {
+function varCheckNum (num) {
     if (num > 10) {
         var isTrue = true;
     } else {
@@ -11,6 +11,47 @@ function checkNum (num) {
     console.log(isTrue);
 }
 
-checkNum(2);
-checkNum(15);
+// varCheckNum(2);
+// varCheckNum(15);
+
+function letCheckSum (num) {
+    if (num > 10) {
+        let isTrue = true;
+    } else {
+        let isTrue = false;
+    }
+    console.log(isTrue);
+}
+
+// letCheckSum(2);
+// letCheckSum(15);
+
+const pi = parseFloat(3.14);
+console.log(pi === parseFloat(3.14));
+
+// const pi = 42; // const error!
+
+// ES5.x
+function addEm (a, b) {
+    console.log(a + b);
+}
+
+let addEm2 = function (a, b) {
+    console.log(a + b);
+}
+
+let langs = ['Swift', 'JavaScript', 'C', 'C++'];
+langs.sort(function (a, b) {
+    if (a > b) {
+        console.log(1);
+    } else if (a < b) {
+        console.log(-1);
+    } else {
+        console.log(0);
+    }
+});
+console.log(langs);
+
+// ...
+
 
